e0de38ba3669039d62ce020fe2bda8ef054dcb00
Ayahiro-Inatsugu <aya13001060ina@gmail.com>
2024-08-05 17:36:02 +0900

    add：DBに接続

--- backend/api/config/config.go
+++ backend/api/config/config.go
@@ -4,11 +4,11 @@ import "github.com/caarlos0/env"
 
 type Config struct {
 	Env string `env:"ENV" envDefault:"dev"`
-	Port string `env:"PORT" envDefault:"8080"`
-	DBHost string `env:"DB_HOST" envDefault:"db"`
+	Port string `env:"PORT" envDefault:"5432"`
+	DBHost string `env:"DB_HOST" envDefault:"localhost"`
 	DBUser string `env:"DB_USER" envDefault:"user"`
 	DBPassword string `env:"DB_PASSWORD" envDefault:"password"`
-	DBName string `env:"DB_NAME" envDefault:"mydb"`
+	DBName string `env:"DB_NAME" envDefault:"onpu"`
 }
 
 func New() (*Config,error) {
