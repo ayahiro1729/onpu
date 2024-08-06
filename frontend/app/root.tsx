782783c2c4f07ca3f9bfdd51413c9df387bb64b5
sekibuuun <akaunto.no.001@gmail.com>
2024-08-06 15:19:21 +0900

    fix: headerの追加

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -6,6 +6,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
+import { Header } from "~/components/Header";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -26,5 +27,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
