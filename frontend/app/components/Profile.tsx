33c5e6a312567a4449a044f2fcaf9267256b3ea6
sekibuuun <akaunto.no.001@gmail.com>
2024-08-07 18:23:52 +0900

    fix: topページ修正

--- frontend/app/components/Profile.tsx
+++ frontend/app/components/Profile.tsx
@@ -15,20 +15,20 @@ const Profile = () => {
       </div>
       <div className='flex justify-between items-center px-2'>
         <div className='flex gap-4 justify-center items-center'>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
+          <Avatar className='w-12 h-12'>
+            <AvatarImage src="https://github.com/shadcn.png"/>
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
           <p className='flex justify-center items-center text-xl'>Name</p>
         </div>
         <div className='flex flex-col gap-2'>
-          <div className='flex gap-2 justify-center items-center'>
+          <div className='flex gap-2 items-center'>
             <img src={X} className='w-5 h-fit'/>
-            <a href='https://x.com' className='text-sm'>@hogehogehoge</a>
+            <a href='https://x.com' className='text-xs'>@hogehoghoge</a>
           </div>
-          <div className='flex gap-2 justify-center items-center'>
+          <div className='flex gap-2 items-center'>
             <img src={Instagram} className='w-5 h-fit'/>
-            <a href='https://www.instagram.com/' className='text-sm'>@hogehogehoge</a>
+            <a href='https://www.instagram.com/' className='text-xs'>@hogehogehoge</a>
           </div>
         </div>
       </div>
