56c7798f33416214a9f6af28c5bfb920671972a4
sekibuuun <akaunto.no.001@gmail.com>
2024-08-07 23:52:59 +0900

    add: favorite music list

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Profile } from "~/components/Profile";
 import { Friends } from "~/components/Friends";
+import { MusicList } from "~/components/MusicList";
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,6 +14,7 @@ export default function Index() {
   return (
     <div className="font-sans p-4 pt-20 flex flex-col gap-8">
       <Profile />
+      <MusicList />
       <Friends />
     </div>
   );
