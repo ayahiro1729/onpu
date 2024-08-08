8e8a8eb0a5dfdda50e2d15a48ff7a8c59cea81aa
sekibuuun <akaunto.no.001@gmail.com>
2024-08-09 00:31:17 +0900

    fix: 修正

--- frontend/app/components/ui/apple-cards-carousel.tsx
+++ frontend/app/components/ui/apple-cards-carousel.tsx
@@ -216,7 +216,7 @@ export const Card = ({
       <motion.button
         layoutId={layout ? `card-${card.title}` : undefined}
         onClick={handleOpen}
-        className="rounded-3xl bg-gray-100 dark:bg-neutral-900 w-56 h-56 md:h-[40rem] md:w-96 overflow-hidden flex flex-col items-start justify-start relative z-10"
+        className="rounded-3xl bg-gray-100 dark:bg-neutral-900 w-56 h-56 overflow-hidden flex flex-col items-start justify-start relative z-10"
       >
         <div className="absolute h-full top-0 inset-x-0 bg-gradient-to-b from-black/50 via-transparent to-transparent z-30 pointer-events-none" />
         <div className="relative z-40 p-8">
