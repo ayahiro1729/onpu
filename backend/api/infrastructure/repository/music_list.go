ef5fdb66a2550242fdcdb99714c28e3319e83b53
hareiharei <harei.whitewings@gmail.com>
2024-08-09 19:46:32 +0900

    musicのGETのためのtypeとinterface定義

---
+++ backend/api/infrastructure/repository/music_list.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"time"
+
+	"domain/model"
+
+	"gorm.io/gorm"
+)
+
+type MusicDTO struct {
+	MusicID 	int 	`json:"music_id"`
+	Name 		string 	`json:"name"`
+	Image 		string 	`json:"image"`
+	ArtistName 	string 	`json:"artist_name"`
+	SpotifyLink string 	`json:"spotify_link"`
+}
+
+type MusicListDTO struct {
+	MusicListID	int			`json:"music_list_id"`
+	CreatedAt	time.Time	`json:"created_at"`
+}
+
+type MusicListWithMusicDTO struct {
+	MusicListDTO
+	Musics		[]MusicDTO	`json:"musics"`
+}
+
+type MusicListRepository interface {
+	LatestMusicList(userID int) (*MusicListDTO, error)
+}
