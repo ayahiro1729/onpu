e4b4da1d561700972e298a4afb1aa6a5b162f0d1
hareiharei <harei.whitewings@gmail.com>
2024-08-10 01:41:58 +0900

    musicのサービス追加

--- backend/api/usecase/service/music_service.go
+++ backend/api/usecase/service/music_service.go
@@ -5,6 +5,22 @@ import (
 	"domain/repository"
 )
 
-type MusicUsecase interface {
-	LatestMusicList() 
+type MusicListUsecase interface {
+	LatestMusicList() (*repository.MusicListWithMusicDTO, error)
+}
+
+type musicListUsecase struct {
+	mlr repository.MusicListRepository
+}
+
+func NewMusicListUsecase(mlr repository.MusicListRepository) MusicListUsecase {
+	return &musicListUsecase{mlr}
+}
+
+func (mlu *musicListUsecase) LatestMusicList() (*repository.MusicListWithMusicDTO, error) {
+	musicList, err := mlu.mlr.LatestMusicList()
+	if err != nil {
+		return nil, err
+	}
+	return musicList, nil
 }
