be77713fbb1cf5536c1137c627ad6c520b27c8f4
sekibuuun <akaunto.no.001@gmail.com>
2024-08-09 13:49:48 +0900

    fix: path

--- frontend/app/routes/ProfileEdit.tsx
+++ frontend/app/routes/ProfileEdit.tsx
@@ -10,7 +10,7 @@ export default function ProfileEdit () {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
-    navigate("/");
+    navigate("/mypage");
   };
   
   return (
