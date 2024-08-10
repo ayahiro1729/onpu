d40beeac684b1756a38678b3f3991c804e55c886
sekibuuun <akaunto.no.001@gmail.com>
2024-08-11 01:06:43 +0900

    feat: APIを一括で叩く

--- frontend/app/routes/user.$user_id.tsx
+++ frontend/app/routes/user.$user_id.tsx
@@ -1,62 +1,74 @@
-import React, { useEffect, useState } from 'react';
+import { json, LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 import { Profile } from "~/components/Profile";
-import { Followings } from "~/components/Followings";
-import { Followers } from "~/components/Followers";
 import { MusicList } from "~/components/MusicList";
-import { useParams } from '@remix-run/react';
-import axios from 'axios';
+import { Follower, Music, UserInfo } from '~/types/types';
+import { Followings } from '~/components/Followings';
+import { Followers } from '~/components/Followers';
 
-type UserData = {
-  displayName: string;
-  iconImage: string;
-  xLink: string;
-  instagramLink: string;
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const userId = params.user_id;
+  const userResponse = await fetch(`http://backend:8080/api/v1/user/${userId}`);
+  const userData = await userResponse.json();
+
+  const musicResponse = await fetch(`http://backend:8080/api/v1/music/${userId}`);
+  const musicData = await musicResponse.json();
+
+  const followerResponse = await fetch(`http://backend:8080/api/v1/follower/${userId}`);
+  const followerData = await followerResponse.json();
+
+  const followingsResponse = await fetch(`http://backend:8080/api/v1/followee/${userId}`);
+  const followingsData = await followingsResponse.json();
+
+  const userInfo: UserInfo = {
+    displayName: userData.user.DisplayName,
+    iconImage: userData.user.IconImage,
+    xLink: userData.user.XLink,
+    instagramLink: userData.user.InstagramLink,
+  };
+
+  const musicList = musicData.musicList.musics.map((music: Music) => {
+    return {
+      src: music.image,
+      title: music.name,
+      category: music.artist_name,
+      content: music.spotify_link,
+    };
+  });
+
+  const followers = followerData.followers.map((follower: Follower) => {
+    return {
+      userId: follower.userId,
+      displayName: follower.displayName,
+      iconImage: follower.iconImage,
+    };
+  });
+
+  const followings = followingsData.followees.map((following: Follower) => {
+    return {
+      userId: following.userId,
+      displayName: following.displayName,
+      iconImage: following.iconImage,
+    };
+  });
+
+  return json({ userInfo, musicList, followers, followings });
 };
 
 export default function User() {
-  const { user_id } = useParams();
-  const [userData, setUserData] = useState<UserData | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const { userInfo, musicList, followers, followings  } = useLoaderData<typeof loader>();
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await axios.get(`http://localhost:8080/api/v1/user/${user_id}`);
-        const user = response.data.user;
-        console.log(user);
-        setUserData({
-          displayName: user.DisplayName,
-          iconImage: user.IconImage,
-          xLink: user.XLink,
-          instagramLink: user.InstagramLink,
-        });
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-        if (axios.isAxiosError(error)) {
-          setError(`Error fetching data: ${error.message}`);
-        } else {
-          setError("An unknown error occurred");
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchUserData();
-  }, [user_id]);
-  
   return (
     <div className="font-sans p-4 pt-20 flex flex-col gap-8">
       <Profile 
-        displayName={userData?.displayName}
-        iconImage={userData?.iconImage}
-        xLink={userData?.xLink}
-        instagramLink={userData?.instagramLink}
+        displayName={userInfo.displayName}
+        iconImage={userInfo.iconImage}
+        xLink={userInfo.xLink}
+        instagramLink={userInfo.instagramLink}
       />
-      {/* <MusicList />
-      <Followings />
-      <Followers /> */}
+      <MusicList musicList={musicList}/>
+      <Followings followings={followings}/>
+      <Followers followers={followers}/>
     </div>
   );
 }
