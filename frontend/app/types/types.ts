c566021af2418102bd25602c8907c54684c42d23
sekibuuun <akaunto.no.001@gmail.com>
2024-08-10 14:58:34 +0900

    型定義

---
+++ frontend/app/types/types.ts
@@ -0,0 +1,14 @@
+export type Follow ={
+  id: number;
+  name: string;
+  designation: string;
+  image: string;
+}
+
+export type FollowData = {
+  user_id: number,
+  user_name: string,
+  display_name: string,
+  icon_image: string,
+  created_at: string
+}
