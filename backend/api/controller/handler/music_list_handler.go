f451e7801dfade3983549c03065e5b87e42846cc
Ayahiro-Inatsugu <aya13001060ina@gmail.com>
2024-08-11 02:07:07 +0900

    add：music_listを作成するAPI（途中）

--- backend/api/controller/handler/music_list_handler.go
+++ backend/api/controller/handler/music_list_handler.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 
 	"github.com/ayahiro1729/onpu/api/usecase/service"
-	
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,7 +22,6 @@ func NewMusicListHandler(musicListService *service.MusicListService) *MusicListH
 
 func (h *MusicListHandler) LatestMusicList(c *gin.Context) {
 	userIDStr := c.Param("user_id")
-	fmt.Printf(userIDStr)
 
 	userID, err := strconv.Atoi(userIDStr)
 	if err != nil {
@@ -40,5 +39,66 @@ func (h *MusicListHandler) LatestMusicList(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"musicList": musicList,
+	})
+}
+
+type MusicListRequest struct {
+	UserID int `json:"user_id"`
+}
+
+// ユーザーのmusic_listを作成
+func (h *MusicListHandler) PostMusicList(c *gin.Context) {
+	var req MusicListRequest
+	//request body からuser_idを取得
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	userID := req.UserID
+
+	// access_tokenを取得
+	token, err := h.musicListService.CheckAccessToken()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// users.idを取得
+	// userID, err := h.musicListService.FetchUserID(token)
+	// if err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// 	return
+	// }
+
+	// ユーザーのお気に入りの曲を10曲取得
+	musics, err := h.musicListService.FetchTenFavoriteMusics(token)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// music_listのデータを作成
+	err = h.musicListService.CreateMusicList(userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// ユーザーの最新のmusic_listのidを取得
+	musicListID, err := h.musicListService.GetLatestMusicListID(userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// musicのデータを作成
+	err = h.musicListService.CreateSingleMusics(musicListID, musics)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "music list created",
 	})
 }
