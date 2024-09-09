8b8d8bd5f0e0693d4d2782cf3132b3cf903cfe6f
Ayahiro-Inatsugu <aya13001060ina@gmail.com>
2024-09-09 23:23:43 +0900

    add：MusicListDTOにUserIDを追加

--- backend/api/infrastructure/repository/music_list_repository.go
+++ backend/api/infrastructure/repository/music_list_repository.go
@@ -5,22 +5,23 @@ import (
 )
 
 type MusicDTO struct {
-	MusicID 	int 	`json:"music_id"`
-	Name 		string 	`json:"name"`
-	Image 		string 	`json:"image"`
-	ArtistName 	string 	`json:"artist_name"`
-	SpotifyLink string 	`json:"spotify_link"`
+	MusicID     int    `json:"music_id"`
+	Name        string `json:"name"`
+	Image       string `json:"image"`
+	ArtistName  string `json:"artist_name"`
+	SpotifyLink string `json:"spotify_link"`
 }
 
 type MusicListDTO struct {
-	MusicListID	int			`json:"music_list_id"`
-	CreatedAt	time.Time	`json:"created_at"`
+	MusicListID int       `json:"music_list_id"`
+	UserID      int       `json:"user_id"`
+	CreatedAt   time.Time `json:"created_at"`
 }
 
 type MusicListWithMusicDTO struct {
-	MusicListID	int			`json:"music_list_id"`
-	CreatedAt	time.Time	`json:"created_at"`
-	Musics		[]MusicDTO	`json:"musics"`
+	MusicListID int        `json:"music_list_id"`
+	CreatedAt   time.Time  `json:"created_at"`
+	Musics      []MusicDTO `json:"musics"`
 }
 
 type MusicListRepository interface {
