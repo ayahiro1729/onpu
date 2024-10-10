2aa73b760f1c991bd1de18659c8cabedc9833b3e
hareiharei <harei.whitewings@gmail.com>
2024-10-10 11:06:08 +0900

    fix: フォロワーとフォロー中の表示されているアイコンが横に並ぶように

--- frontend/app/components/Followers.tsx
+++ frontend/app/components/Followers.tsx
@@ -7,11 +7,13 @@ export const Followers: React.FC<FollowerProps> = ({followers}) => {
     <div className="flex flex-col gap-4">
       <p className="text-2xl">Followers</p>
       { followers.length === 0 && <div>No followers found.</div> }
+      <div className="flex gap-2">
       { followers.map((follower, index) => (
         <Link to={`/user/${follower.user_id}`} key={index} className="flex items-center gap-2">
           <img src={follower.icon_image} className="w-10 h-10 rounded-full" />
         </Link>
       ))}
+      </div>
     </div>
   );
 }
--- frontend/app/components/Followings.tsx
+++ frontend/app/components/Followings.tsx
@@ -7,11 +7,13 @@ export const Followings: React.FC<FollowingsProps> = ({followings}) => {
     <div className="flex flex-col gap-4">
       <p className="text-2xl">Followings</p>
       { followings.length === 0 && <div>No followings found.</div> }
+      <div className="flex gap-2">
       { followings.map((following, index) => (
         <Link to={`/user/${following.user_id}`} key={index} className="flex items-center gap-2">
           <img src={following.icon_image} className="w-10 h-10 rounded-full" />
         </Link>
       ))}
+      </div>
     </div>
   );
 }
